8d5db6f3f6536593e0126310ae90a54d592ca360
Boris <Boris@gmail.com>
2023-06-11 15:27:31 +0300

    Examle004_Div

--- Examle004_Div/Program.cs
+++ Examle004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 10;
+double numberB = 5;
+double result = numberA / numberB;
+Console.WriteLine (result);
